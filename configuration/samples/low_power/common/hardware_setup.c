de1a052534c2e27b2e6f4273bacc9296eb99cef6
Phuong Nam Nguyen <phuong-nam.nguyen.uj@renesas.com>
2022-05-27 23:49:20 +0800

    Add Low Power sample

---
+++ configuration/samples/low_power/common/hardware_setup.c
@@ -0,0 +1,27 @@
+
+#include "r_smc_entry.h"
+
+#include <platform.h>
+
+#include <r_cmt_rx_if.h>
+
+#include "tx_api.h"
+
+#include "hardware_setup.h"
+
+void _tx_timer_interrupt(void);
+uint32_t chan;
+
+/* CMT Timer callback used as the system tick. */
+void timer_callback(void * pdata)
+{
+    _tx_timer_interrupt();
+}
+
+
+void platform_setup(void)
+{
+    /* Create periodic timer for the system tick. */
+    R_CMT_CreatePeriodic(TX_TIMER_TICKS_PER_SECOND, timer_callback, &chan);
+    
+}
